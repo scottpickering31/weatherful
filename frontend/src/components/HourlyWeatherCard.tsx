feb16733fcbc2b753a5304016f0cdb97fa16bc4a
Scott Pickering <scottpickering31@gmail.com>
2024-03-09 23:10:46 +0000

    New: Added Local state to change hourlyData variable information and allow user to toggle between 1 hour intervals

--- frontend/src/components/HourlyWeatherCard.tsx
+++ frontend/src/components/HourlyWeatherCard.tsx
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchWeatherData } from "../state/reducers/weatherDataSlice";
 import Skeleton from "react-loading-skeleton";
 import { format } from "date-fns";
 import "react-loading-skeleton/dist/skeleton.css";
 
 function HourlyWeatherCard() {
+  const [counter, setCounter] = useState(1);
   const dispatch = useDispatch();
   const weatherData = useSelector((state) => state.weatherData.weatherData);
 
@@ -15,8 +16,21 @@ function HourlyWeatherCard() {
 
   const hourlyData = weatherData
     ? weatherData.locations[Object.keys(weatherData.locations)[0]].values
-    : [];
-  console.log(hourlyData);
+    : null;
+
+  const hourlyDataMap = hourlyData
+    ? hourlyData.slice(0 + counter, 1 + counter)
+    : null;
+
+  console.log(hourlyDataMap);
+
+  function prevHour() {
+    setCounter((prevCounter) => prevCounter - 1);
+  }
+
+  function nextHour() {
+    setCounter((prevCounter) => prevCounter + 1);
+  }
 
   const setDate = () => {
     const rawDate = weatherData
@@ -33,19 +47,28 @@ function HourlyWeatherCard() {
   };
 
   return (
-    <div className="border-4 border-slate-300 rounded-xl flex items-center h-small w-full">
-      {weatherData ? (
-        <div className="flex flex-row items-center">
-          <div className="border-r-2 p-5 flex flex-col gap-5 w-1/2">
+    <div className="border-4 border-slate-300 rounded-xl flex flex-col items-center h-small w-full">
+      {hourlyData ? (
+        hourlyDataMap.map((data, index) => (
+          <div className="flex flex-col items-center">
             <div className="text-center">
               <h2 className="font-semibold text-xl">{setDate()}</h2>
             </div>
-            <p>Gerrofff</p>
+            <div className="text-center">
+              <h3 className="font-semibold text-xl">Time</h3>
+            </div>
+            <div className="p-5 flex flex-row gap-5 w-1/2">
+              <div>
+                <p>Gerrofff</p>
+              </div>
+            </div>
+            <div className="p-5 w-1/2 text-center flex justify-around font-bold items-start flex-col gap-10">
+              <p>Data</p>
+            </div>
+            <button onClick={nextHour}>+</button>
+            <button onClick={prevHour}>-</button>
           </div>
-          <div className="p-5 w-1/2 text-center flex justify-around font-bold items-start flex-col gap-10">
-            <p>Data</p>
-          </div>
-        </div>
+        ))
       ) : (
         <div className="flex flex-row items-center m-12">
           <div className="p-5 flex flex-col gap-10 w-1/2">
